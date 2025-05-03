6cf0a68b8d15c5c9d8db049a16d76cc37ca04b05
STEVAN ZAKY S <Stevanzaky87@gmail.com>
2025-05-03 22:29:08 +0700

    feat: add hero section, login, register page

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,5 +1,16 @@
-<flux:navbar class="bg-dark-cyan-blue px-6 fixed top-0 w-full z-50">
+<flux:navbar class="px-6 fixed top-0 w-full z-50 flex justify-between">
     <flux:navbar.item href="{{ route('home') }}">
-        <flux:text class="font-bold text-white text-2xl">{{ config('app.name') }}</flux:text>
+        <flux:text class="font-black text-black text-2xl">{{ config('app.name') }}</flux:text>
     </flux:navbar.item>
+    <div class="flex">
+        <flux:navbar.item href="{{ route('home') }}">
+            <flux:text class=" text-black text-lg">Tentang</flux:text>
+        </flux:navbar.item>
+        <flux:navbar.item href="{{ route('home') }}">
+            <flux:text class=" text-black text-lg">Panduan</flux:text>
+        </flux:navbar.item>
+        <flux:navbar.item href="{{ route('home') }}">
+            <flux:text class=" text-black text-lg">Hubungi Kami</flux:text>
+        </flux:navbar.item>
+    </div>
 </flux:navbar>
