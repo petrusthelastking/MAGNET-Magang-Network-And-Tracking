8693e922457ae6b19f5c3a8710e306a3fe329e4f
arilibbetardanaputra <arilardana111@gmail.com>
2025-05-11 21:13:04 +0700

    Membuat dashboard mahasiswa

---
+++ resources/views/components/mahasiswa/sidebar.blade.php
@@ -0,0 +1,46 @@
+<flux:sidebar stashable
+    class="bg-zinc-50 dark:bg-zinc-900 border-r rtl:border-r-0 rtl:border-l border-zinc-200 dark:border-zinc-700">
+    <flux:sidebar.toggle class="lg:hidden" icon="x-mark" />
+    <a href="#">
+        <H1 class="text-3xl font-black ps-2 mx-4 text-center text-[#219EBC]">MAGNET</H1>
+    </a>
+    <flux:navlist variant="outline">
+        <flux:navlist.item icon="home" href="{{ route('dashboard') }}" current>
+            Dashboard
+        </flux:navlist.item>
+       
+        <flux:navlist.item icon="flask-conical" href="{{ route('dashboard') }}">
+            Pengajuan Magang
+        </flux:navlist.item>
+
+        <flux:navlist.item icon="battery-medium" href="{{ route('dashboard') }}">
+            Pembaruan Status
+        </flux:navlist.item>
+
+        <flux:navlist.item icon="file-chart-column-increasing" href="{{ route('dashboard') }}">
+            Log Magang
+        </flux:navlist.item>
+        
+    </flux:navlist>
+
+    <flux:spacer />
+
+    <flux:dropdown position="top" align="start" class="max-lg:hidden">
+        <flux:profile avatar="https://fluxui.dev/img/demo/user.png" name="Olivia Martin" />
+        <flux:menu>
+            <flux:menu.radio.group>
+                <flux:menu.radio checked>Olivia Martin</flux:menu.radio>
+                <flux:menu.radio>Truly Delta</flux:menu.radio>
+            </flux:menu.radio.group>
+            
+            <flux:menu.separator />
+
+            <form method="POST" action="{{ route('logout') }}" class="w-full">
+                @csrf
+                <flux:menu.item as="button" type="submit" icon="arrow-right-start-on-rectangle" class="w-full">
+                    {{ __('Log Out') }}
+                </flux:menu.item>
+            </form>
+        </flux:menu>
+    </flux:dropdown>
+</flux:sidebar>
