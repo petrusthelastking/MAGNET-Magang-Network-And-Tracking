8bbdd6b3d71fb656f00b136a612a1a3b34111db3
arilibbetardanaputra <arilardana111@gmail.com>
2025-05-13 21:47:22 +0700

    Add setting profile & log mahasiswa page

--- resources/views/Mahasiswa/setting_profile.blade.php
+++ resources/views/Mahasiswa/setting_profile.blade.php
@@ -1,69 +1,71 @@
-<x-layouts.mahasiswa.mahasiswa >
+<x-layouts.mahasiswa.mahasiswa>
     <flux:breadcrumbs class="mb-5">
         <flux:breadcrumbs.item href="{{ route('mahasiswa.setting-profile') }}" icon="home" icon:variant="outline" />
         <flux:breadcrumbs.item href="{{ route('mahasiswa.setting-profile') }}" class="text-black">Setting Profile</flux:breadcrumbs.item>
     </flux:breadcrumbs>
 
     <div class="bg-[#E0F7FA] min-h-screen p-6">
-    <div class="max-w-3xl mx-auto bg-white p-6 rounded-lg shadow">
-        <h2 class="text-lg font-semibold mb-4 text-black">Data Diri Mahasiswa</h2>
+        <div class="max-w-3xl mx-auto bg-white p-6 rounded-lg shadow">
+            <h2 class="text-lg font-semibold mb-4 text-black">Data Diri Mahasiswa</h2>
 
-        <form class="space-y-4 text-black">
-            <!-- Nama -->
-            <div>
-                <label class="block font-medium mb-1">Nama</label>
-                <input type="text" name="nama" class="w-full rounded-md border-gray-300 shadow-sm text-black focus:ring focus:ring-green-200 focus:border-green-400" />
-            </div>
+            <form class="space-y-4 text-black">
 
-            <!-- NIM -->
-            <div>
-                <label class="block font-medium mb-1">NIM</label>
-                <input type="text" name="nim" class="w-full rounded-md border-gray-300 shadow-sm text-black focus:ring focus:ring-green-200 focus:border-green-400" />
-            </div>
+                <!-- Nama -->
+                <div class="flex items-center gap-2 p-[6px_13px] align-self-stretch">
+                    <label class="w-1/4 font-medium">Nama</label>
+                    <input type="text" name="nama" class="w-full rounded-md border-gray-300 shadow-sm text-black" />
+                </div>
 
-            <!-- Jurusan -->
-            <div>
-                <label class="block font-medium mb-1">Jurusan</label>
-                <input type="text" name="jurusan" class="w-full rounded-md border-gray-300 shadow-sm text-black focus:ring focus:ring-green-200 focus:border-green-400" />
-            </div>
+                <!-- NIM -->
+                <div class="flex items-center gap-2 p-[6px_13px] align-self-stretch">
+                    <label class="w-1/4 font-medium">NIM</label>
+                    <input type="text" name="nim"
+                    oninput="this.value=this.value.replace(/[^0-9]/g,'')"
+                    class="w-full rounded-md border-gray-300 shadow-sm text-black bg-white/50 focus:ring focus:ring-green-200 focus:border-green-400" />
+                </div>
 
-            <!-- Program Studi -->
-            <div>
-                <label class="block font-medium mb-1">Program Studi</label>
-                <input type="text" name="prodi" class="w-full rounded-md border-gray-300 shadow-sm text-black focus:ring focus:ring-green-200 focus:border-green-400" />
-            </div>
+                <!-- Jurusan -->
+                <div class="flex items-center gap-2 p-[6px_13px] align-self-stretch">
+                    <label class="w-1/4 font-medium">Jurusan</label>
+                    <input type="text" name="jurusan" class="w-full rounded-md border-gray-300 shadow-sm text-black" />
+                </div>
 
-            <!-- Jenis Kelamin -->
-            <div>
-                <label class="block font-medium mb-1">Jenis Kelamin</label>
-                <select name="jenis_kelamin" class="w-full rounded-md border-gray-300 shadow-sm text-black focus:ring focus:ring-green-200 focus:border-green-400">
-                    <option value="" disabled selected>Pilih jenis kelamin</option>
-                    <option value="Laki-laki">Laki-laki</option>
-                    <option value="Perempuan">Perempuan</option>1
-                </select>
-            </div>
+                <!-- Program Studi -->
+                <div class="flex items-center gap-2 p-[6px_13px] align-self-stretch">
+                    <label class="w-1/4 font-medium">Program Studi</label>
+                    <input type="text" name="prodi" class="w-full rounded-md border-gray-300 shadow-sm text-black" />
+                </div>
 
-            <!-- Alamat -->
-            <div>
-                <label class="block font-medium mb-1">Alamat</label>
-                <textarea name="alamat"
-                        class="w-full rounded-md border-gray-300 shadow-sm text-black focus:ring focus:ring-green-200 focus:border-green-400 resize-y min-h-[80px]"></textarea>
-            </div>
+                <!-- Jenis Kelamin -->
+                <div class="flex items-center gap-2 p-[6px_13px] align-self-stretch">
+                    <label class="w-1/4 font-medium">Jenis Kelamin</label>
+                    <select name="jenis_kelamin" class="w-full rounded-md border-gray-300 shadow-sm text-black">
+                        <option value="" disabled selected>Pilih jenis kelamin</option>
+                        <option value="Laki-laki">Laki-laki</option>
+                        <option value="Perempuan">Perempuan</option>
+                    </select>
+                </div>
 
-            <!-- Prestasi -->
-            <div>
-                <label class="block font-medium mb-1">Prestasi </label>
-                <input type="text" name="prestasi_" class="w-full rounded-md border-gray-300 shadow-sm text-black focus:ring focus:ring-green-200 focus:border-green-400" />
-            </div>
+                <!-- Alamat -->
+                <div class="flex items-center gap-2 p-[6px_13px] align-self-stretch">
+                    <label class="w-1/4 font-medium">Alamat</label>
+                    <textarea name="alamat" class="w-full rounded-md border-gray-300 shadow-sm text-black min-h-[80px] resize-y"></textarea>
+                </div>
 
-            <!-- Tombol Simpan -->
-            <div class="flex justify-end pt-4">
-                <button type="submit" class="bg-green-500 hover:bg-green-600 text-white px-6 py-2 rounded-md shadow">
-                    Simpan
-                </button>
-            </div>
-        </form>
+                <!-- Prestasi -->
+                <div class="flex items-center gap-2 p-[6px_13px] align-self-stretch">
+                    <label class="w-1/4 font-medium">Prestasi</label>
+                    <input type="text" name="prestasi_1" class="w-full rounded-md border-gray-300 shadow-sm text-black" />
+                </div>
+
+                <!-- Tombol Simpan -->
+                <div class="flex justify-end pt-4">
+                    <button type="submit" class="bg-green-500 hover:bg-green-600 text-white px-6 py-2 rounded-md shadow">
+                        Simpan
+                    </button>
+                </div>
+
+            </form>
+        </div>
     </div>
-</div>
-
 </x-layouts.mahasiswa.mahasiswa>
