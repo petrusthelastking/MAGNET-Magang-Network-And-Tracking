2f030869c6a65b7b0696c8ccb91f85c113fa04b2
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2025-05-16 17:36:15 +0700

    add initial setup for laravel v10

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,17 +6,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Support\Str;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
      *
-     * @var list<string>
+     * @var array<int, string>
      */
     protected $fillable = [
         'name',
@@ -27,7 +26,7 @@ class User extends Authenticatable
     /**
      * The attributes that should be hidden for serialization.
      *
-     * @var list<string>
+     * @var array<int, string>
      */
     protected $hidden = [
         'password',
@@ -35,26 +34,11 @@ class User extends Authenticatable
     ];
 
     /**
-     * Get the attributes that should be cast.
+     * The attributes that should be cast.
      *
-     * @return array<string, string>
+     * @var array<string, string>
      */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
-    }
-
-    /**
-     * Get the user's initials
-     */
-    public function initials(): string
-    {
-        return Str::of($this->name)
-            ->explode(' ')
-            ->map(fn (string $name) => Str::of($name)->substr(0, 1))
-            ->implode('');
-    }
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
 }
--- routes/web.php
+++ routes/web.php
@@ -1,34 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Livewire\Volt\Volt;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
-Route::get('/', fn() => view('home'))
-    ->name('home');
-
-Route::middleware(['auth'])->group(function () {
-    Route::redirect('settings', 'settings/profile');
-
-    Volt::route('settings/profile', 'settings.profile')->name('settings.profile');
-    Volt::route('settings/password', 'settings.password')->name('settings.password');
-    Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
-
-    Route::view('dashboard', 'admin.dashboard')
-        ->name('dashboard');
-    Route::view('students-data', 'admin.students-data')
-        ->name('students-data');
+Route::get('/', function () {
+    return view('welcome');
 });
-
-require __DIR__.'/auth.php';
-
-    Route::view('mahasiswa/dashboard', 'mahasiswa.dashboard')
-        ->name('mahasiswa.dashboard');
-    Route::view('mahasiswa/pengajuan-magang', 'mahasiswa.pengajuan_magang')
-        ->name('mahasiswa.pengajuan-magang');
-    Route::view('mahasiswa/pembaruan-status', 'mahasiswa.pembaruan_status')
-        ->name('mahasiswa.pembaruan-status');
-    Route::view('mahasiswa/log-mahasiswa', 'mahasiswa.log_mahasiswa')
-        ->name('mahasiswa.log-mahasiswa');
-    Route::view('mahasiswa/setting-profile', 'mahasiswa.setting_profile')
-        ->name('mahasiswa.setting-profile');
