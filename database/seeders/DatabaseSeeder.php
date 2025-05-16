88fc3d20e15db37074e74afdb66b942edaa7ed09
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2025-05-16 17:36:15 +0700

    add initial setup for laravel v10

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,19 +12,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call([
-            ProgramStudiSeeder::class,
-            UserSeeder::class,
-            MahasiswaProfileSeeder::class,
-            DosenProfileSeeder::class,
-            AdminProfileSeeder::class,
-            KeahlianSeeder::class,
-            PreferensiKeahlianSeeder::class,
-            PerusahaanSeeder::class,
-            LowonganMagangSeeder::class,
-            PengajuanMagangSeeder::class,
-            LogAktivitasMagangSeeder::class,
-            UlasanMagangSeeder::class,
-        ]);
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
     }
 }
