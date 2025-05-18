c95eddd66ce47bcd18db6441759ddc99be452ec9
arilibbetardanaputra <arilardana111@gmail.com>
2025-05-18 18:35:09 +0700

    Adjustment at pengajuan magang page

--- resources/views/pages/mahasiswa/pengajuan_magang.blade.php
+++ resources/views/pages/mahasiswa/pengajuan_magang.blade.php
@@ -7,10 +7,13 @@
 
     <div class="card bg-white shadow-md">
         <div class="card-body">
-            <flux:input type="text" label="Nama Lengkap" />
-            <flux:input type="text" label="Jurusan" />
-            <flux:input type="text" label="Program Studi" />
+            <flux:input value="Yanto" type="text" label="Nama Lengkap" />
+            <flux:input readonly value="Teknologi Informasi" type="text" label="Jurusan" />
+            <flux:input value="D4 Teknik Informatika" type="text" label="Program Studi" />
+            <flux:input type="file" wire:model="attachments" label="Transkrip Nilai" />
             <flux:input type="file" wire:model="attachments" label="CV" />
+            <flux:input type="file" wire:model="attachments" label="Portofolio" />
+            <flux:input type="file" wire:model="attachments" label="Sertifikat" />
         </div>
 
         <div class="card-actions flex justify-end p-5">
