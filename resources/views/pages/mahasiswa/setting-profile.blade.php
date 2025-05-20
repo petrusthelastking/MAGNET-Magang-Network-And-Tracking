00685264fbea85a877865ce50b1c000d21e1e1fb
arilibbetardanaputra <arilardana111@gmail.com>
2025-05-20 21:24:32 +0700

    refactor: update mahasiswa profile view to use correct data attributes and improve layout

--- resources/views/pages/mahasiswa/setting-profile.blade.php
+++ resources/views/pages/mahasiswa/setting-profile.blade.php
@@ -2,37 +2,35 @@
     <div class="gap-3 flex flex-col">
         <div class="card bg-white shadow-md">
             <div class="card-body p-5">
-                <img src="{{ $mahasiswa->foto_path ? asset('storage/' . $mahasiswa->foto_path) : 'https://unavatar.io/x/' . $user->name }}" class="w-24 h-24 rounded-full" alt="Avatar" />
-                <div class="grid grid-cols-2 gap-3 mt-4">
-                    <flux:input readonly value="{{ $user->name }}" type="text" label="Nama Lengkap" />
+                <flux:avatar circle src="https://unavatar.io/x/{{ $mahasiswa->name }}" class="w-24 h-24" />
+                <div class="grid grid-cols-2 gap-3 ">
+                    <flux:input readonly value="{{ $mahasiswa->name }}" type="text" label="Nama Lengkap" />
                     <flux:input readonly value="{{ $mahasiswa->nim }}" type="text" label="NIM" />
                     <flux:input readonly value="{{ $mahasiswa->jurusan }}" type="text" label="Jurusan" />
-                    <flux:input readonly value="{{ $mahasiswa->programStudi->nama ?? '-' }}" type="text" label="Program Studi" />
+                    <flux:input readonly value="{{ $mahasiswa->prodi }}" type="text" label="Program Studi" />
                     <flux:input readonly value="{{ $mahasiswa->jenis_kelamin }}" type="text" label="Jenis Kelamin" />
-                    <flux:input readonly value="{{ $mahasiswa->alamat ?? '-' }}" type="text" label="Alamat" />
+                    <flux:input readonly value="{{ $mahasiswa->alamat }}" type="text" label="Alamat" />
                 </div>
             </div>
 
             <div class="card-actions flex justify-end p-5">
-                <a href="{{ route('mahasiswa.profile.edit') }}">
-                    <flux:button class="bg-magnet-sky-teal! text-white!" icon="pencil">Edit data anda</flux:button>
-                </a>
+                <flux:button class="bg-magnet-sky-teal! text-white!" icon="pencil">Edit data anda</flux:button>
             </div>
         </div>
 
         <div class="card bg-white shadow-md">
             <div class="card-body grid grid-cols-2 gap-3">
-                <flux:input readonly value="{{ $preferensi->keahlian ?? '-' }}" type="text" label="Skill yang anda miliki" />
-                <flux:input readonly value="{{ $preferensi->bidang_inustri ?? '-' }}" type="text" label="Bidang industri" />
-                <flux:input readonly value="{{ $preferensi->pekerjaan_impian ?? '-' }}" type="text" label="Pekerjaan impian" />
-                <flux:input readonly value="{{ number_format($preferensi->upah_minimum ?? 0, 0, ',', '.') }}" type="text" label="Upah minimum" />
-                <flux:input readonly value="{{ $preferensi->lokasi_magang ?? '-' }}" type="text" label="Lokasi magang" />
+                <flux:input readonly value="{{ $mahasiswa->skill }}" type="text" label="Skill yang anda miliki" />
+                <flux:input readonly value="{{ $mahasiswa->bidang_industri }}" type="text" label="Bidang industri" />
+                <flux:input readonly value="{{ $mahasiswa->pekerjaan_impian }}" type="text"
+                    label="Pekerjaan impian" />
+                <flux:input readonly value="{{ $mahasiswa->upah_minimum }}" type="text" label="Upah minimum" />
+                <flux:input readonly value="{{ $mahasiswa->lokasi_magang }}" type="text" label="Lokasi magang" />
             </div>
 
             <div class="card-actions flex justify-end p-5">
-                <a href="{{ route('mahasiswa.preference.edit') }}">
-                    <flux:button class="bg-magnet-sky-teal! text-white!" icon="pencil">Edit data preferensi magang</flux:button>
-                </a>
+                <flux:button class="bg-magnet-sky-teal! text-white!" icon="pencil">Edit data preferensi magang
+                </flux:button>
             </div>
         </div>
     </div>
