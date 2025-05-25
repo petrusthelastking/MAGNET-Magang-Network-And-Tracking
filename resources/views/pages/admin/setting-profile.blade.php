c9539de33f04c5ef038eb9680b84d270fdbc1114
STEVAN ZAKY S <Stevanzaky87@gmail.com>
2025-05-25 14:26:05 +0700

    fixed router

--- resources/views/pages/admin/setting-profile.blade.php
+++ resources/views/pages/admin/setting-profile.blade.php
@@ -1,1 +1,25 @@
-<h1>setting profile</h1>
+<x-layouts.admin.main>
+    <flux:breadcrumbs>
+        <flux:breadcrumbs.item href="{{ route('dashboard') }}" icon="home" icon:variant="outline" />
+        <flux:breadcrumbs.item href="{{ route('admin.setting-profile') }}" class="text-black">Profil Anda
+        </flux:breadcrumbs.item>
+    </flux:breadcrumbs>
+    <div class="gap-3 flex flex-col">
+        <div class="card bg-white shadow-md">
+            <div class="card-body p-5">
+                <flux:avatar circle src="https://unavatar.io/x/Jane Doe" class="w-24 h-24" />
+                <div class="grid grid-cols-2 gap-3 ">
+                    <flux:input readonly value="Jane Doe" type="text" label="Nama Lengkap" />
+                    <flux:input readonly value="1234567890" type="text" label="NIP" />
+                    <flux:input readonly value="Laki Laki" type="text" label="Jenis Kelamin" />
+                    <flux:input readonly value="Jl. Mawar No. 12, Malang" type="text" label="Alamat" />
+                </div>
+            </div>
+
+            <div class="card-actions flex justify-end p-5">
+                <flux:button class="bg-magnet-sky-teal! text-white!" icon="pencil">Edit data anda</flux:button>
+            </div>
+        </div>
+
+    </div>
+</x-layouts.admin.main>
