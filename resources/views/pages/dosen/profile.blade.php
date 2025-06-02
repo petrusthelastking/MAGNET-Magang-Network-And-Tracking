e18bfc1816ab83cfe17be547d2861b67fd42de13
saalsabl <aisyahnzkk@gmail.com>
2025-06-02 12:08:33 +0700

    Melengkapi page profil dosen

--- resources/views/pages/dosen/profile.blade.php
+++ resources/views/pages/dosen/profile.blade.php
@@ -5,6 +5,7 @@
         </flux:breadcrumbs.item>
     </flux:breadcrumbs>
 
+    <h1 class="text-base font-bold leading-6 text-black">Profil Anda</h1>
     <div class="gap-3 flex flex-col">
         <div class="card bg-white shadow-md">
             <div class="card-body p-5">
@@ -12,9 +13,10 @@
                 <div class="grid grid-cols-2 gap-3 ">
                     <flux:input readonly value="{{ auth('dosen')->user()->nama }}" type="text" label="Nama Lengkap" />
                     <flux:input readonly value="{{ auth('dosen')->user()->nidn }}" type="text" label="NIDN" />
+                    <flux:input readonly value="{{ auth('dosen')->user()->jenis_kelamin == 'L' ? 'Laki-Laki' : (auth('dosen')->user()->jenis_kelamin == 'P' ? 'Perempuan' : '-')}}" type="text" label="Jenis Kelamin" />
                 </div>
             </div>
-
+            
             <div class="card-actions flex justify-end p-5">
                 <flux:button class="bg-magnet-sky-teal! text-white!" icon="pencil">Edit data anda</flux:button>
             </div>
