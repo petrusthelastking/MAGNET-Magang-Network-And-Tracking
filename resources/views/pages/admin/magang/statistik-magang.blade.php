3a06184ab86f868f9c8018a21a5dfe023a53cac3
STEVAN ZAKY S <Stevanzaky87@gmail.com>
2025-06-03 15:27:35 +0700

    fixed code statistik magang

--- resources/views/pages/admin/magang/statistik-magang.blade.php
+++ resources/views/pages/admin/magang/statistik-magang.blade.php
@@ -7,40 +7,40 @@
     <h1 class="text-base font-bold leading-6 text-black">Statistik Magang</h1>
     <div class="grid grid-cols-3 gap-4">
         <flux:button href="{{ route('admin.perusahaan-terpopuler') }}"
-            class="!bg-magnet-def-red-300 !text-white !rounded-md !shadow-lg !h-28 !p-4 !flex !items-center !gap-3 !justify-start">
-            <flux:icon.building class="!w-12 !h-12" />
-            <p class="!text-lg">Perusahaan Terpopuler</p>
+            class="bg-magnet-def-red-300! text-white! rounded-md shadow-lg h-28 p-4 flex items-center gap-3 justify-start">
+            <flux:icon.building class="w-12 h-12" />
+            <p class="text-lg">Perusahaan Terpopuler</p>
             
         </flux:button>
 
         <flux:button
-            class="!bg-magnet-def-red-300 !text-white !rounded-md !shadow-lg !h-28 !p-4 !flex !items-center !gap-3 !justify-start">
-            <flux:icon.building class="!w-12 !h-12" />
-            <p class="!text-lg">Perusahaan Paling Direkomendasikan</p>
+            class="bg-magnet-def-red-300! text-white! rounded-md shadow-lg h-28 p-4 flex items-center gap-3 justify-start">
+            <flux:icon.building class="w-12 h-12" />
+            <p class="text-lg">Perusahaan Paling Direkomendasikan</p>
         </flux:button>
 
         <flux:button
-            class="!bg-magnet-def-yellow-300 !text-white !rounded-md !shadow-lg !h-28 !p-4 !flex !items-center !gap-3 !justify-start">
-            <flux:icon.circle-dot class="!w-12 !h-12" />
-            <p class="!text-lg">Bidang Terfavorit</p>
+            class="bg-magnet-def-yellow-300! text-white! rounded-md shadow-lg h-28 p-4 flex items-center gap-3 justify-start">
+            <flux:icon.circle-dot class="w-12 h-12" />
+            <p class="text-lg">Bidang Terfavorit</p>
         </flux:button>
 
         <flux:button
-            class="!bg-magnet-def-green-300 !text-white !rounded-md !shadow-lg !h-28 !p-4 !flex !items-center !gap-3 !justify-start">
-            <flux:icon.map-pinned class="!w-12 !h-12" />
-            <p class="!text-lg">Lokasi Magang</p>
+            class="bg-magnet-def-green-300! text-white! rounded-md shadow-lg h-28 p-4 flex items-center gap-3 justify-start">
+            <flux:icon.map-pinned class="w-12 h-12" />
+            <p class="text-lg">Lokasi Magang</p>
         </flux:button>
 
         <flux:button
-            class="!bg-magnet-def-purple-300 !text-white !rounded-md !shadow-lg !h-28 !p-4 !flex !items-center !gap-3 !justify-start">
-            <flux:icon.circuit-board class="!w-12 !h-12" />
-            <p class="!text-lg">Pemetaan skil dengan perusahaan</p>
+            class="bg-magnet-def-purple-300! text-white! rounded-md shadow-lg h-28 p-4 flex items-center gap-3 justify-start">
+            <flux:icon.circuit-board class="w-12 h-12" />
+            <p class="text-lg">Pemetaan skil dengan perusahaan</p>
         </flux:button>
 
         <flux:button
-            class="!bg-magnet-def-indigo-300 !text-white !rounded-md !shadow-lg !h-28 !p-4 !flex !items-center !gap-3 !justify-start">
-            <flux:icon.crown class="!w-12 !h-12" />
-            <p class="!text-lg">Top performa mahasiswa magang</p>
+            class="bg-magnet-def-indigo-300! text-white! rounded-md shadow-lg h-28 p-4 flex items-center gap-3 justify-start">
+            <flux:icon.crown class="w-12 h-12" />
+            <p class="text-lg">Top performa mahasiswa magang</p>
         </flux:button>
 
 
