c714bc2652e16f03823fc94a962c804cc1cfcd9a
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2025-06-04 11:26:01 +0700

    fix: layout page `detail-perusahaan`

--- resources/views/pages/mahasiswa/dashboard.blade.php
+++ resources/views/pages/mahasiswa/dashboard.blade.php
@@ -18,7 +18,7 @@ mount(function () {
             Pemberitahuan Terbaru
         </h2>
         <div
-            class="w-full h-[162px] flex-shrink-0 rounded-[15px] bg-white s p-4 flex flex-col justify-between">
+            class="w-full h-40 flex-shrink-0 rounded-lg bg-white s p-4 flex flex-col justify-between shadow-lg">
             <p class="text-black text-base font-medium">
                 Isi pemberitahuan terbaru
             </p>
@@ -26,151 +26,30 @@ mount(function () {
 
         <div class="w-full flex justify-center">
             <form action="{{ route('mahasiswa.search') }}" method="GET" class="flex w-full justify-center">
-                <flux:input icon="magnifying-glass" name="query" class="w-114!"
+                <flux:input type="submit" icon="magnifying-glass" name="query" class="w-1/3!"
                     placeholder="Cari Tempat Magang Yang Anda Inginkan" />
             </form>
         </div>
 
-        <!-- Bottom content card -->
-        <h2
-            class="text-[18px] font-extrabold leading-6 text-black w-[570.126px] h-[27px] flex-shrink-0 font-['Inter'] mt-6">
-            Rekomendasi Tempat Magang
-        </h2>
-        <div class="grid grid-cols-3 gap-3">
-            <button onclick="window.location='{{ route('mahasiswa.detail-perusahaan') }}'"
-                class="text-black text-base font-medium no-underline w-full text-left">
-                <div class="card-body bg-white hover:bg-gray-100 transition-colors rounded-md">
-                    <div class="flex align-middle items-center">
-                        <img src="{{ asset('logo-perusahaan.png') }}" alt="Logo Perusahaan"
-                            class="w-10 h-10 object-contain">
-                        <p class="h-fit px-3">PT. Kimia Farma</p>
-                    </div>
-                    <div class="font-bold text-lg mt-2">
-                        <p>Frontend Web Developer</p>
-                    </div>
-                    <p class="text-sm text-gray-600">Jakarta</p>
-                </div>
-            </button>
+        <h2 class="font-extrabold leading-6 text-black w-full">Rekomendasi Tempat Magang</h2>
 
-            <button onclick="window.location='{{ route('mahasiswa.detail-perusahaan') }}'"
-                class="text-black text-base font-medium no-underline w-full text-left">
+        <div class="grid grid-cols-1 md:grid-cols-3 gap-3">
+            @for ($i = 0; $i < 21; $i++)
+            <div onclick="window.location='{{ route('mahasiswa.detail-perusahaan') }}'" role="button"
+                class="card shadow-lg hover:cursor-pointer">
                 <div class="card-body bg-white hover:bg-gray-100 transition-colors rounded-md">
-                    <div class="flex align-middle items-center">
-                        <img src="{{ asset('logo-perusahaan.png') }}" alt="Logo Perusahaan"
+                    <div class="flex align-middle items-center gap-4">
+                        <img src="{{ asset('img/company-kimia-farma.png') }}" alt="Logo Perusahaan"
                             class="w-10 h-10 object-contain">
-                        <p class="h-fit px-3">PT. Kimia Farma</p>
+                        <p>PT. Kimia Farma</p>
                     </div>
-                    <div class="font-bold text-lg mt-2">
-                        <p>Frontend Web Developer</p>
+                    <div>
+                        <p class="font-bold text-base">Frontend Web Developer</p>
+                        <p class="text-sm text-gray-600">Jakarta</p>
                     </div>
-                    <p class="text-sm text-gray-600">Jakarta</p>
                 </div>
-            </button>
-
-            <button onclick="window.location='{{ route('mahasiswa.detail-perusahaan') }}'"
-                class="text-black text-base font-medium no-underline w-full text-left">
-                <div class="card-body bg-white hover:bg-gray-100 transition-colors rounded-md">
-                    <div class="flex align-middle items-center">
-                        <img src="{{ asset('logo-perusahaan.png') }}" alt="Logo Perusahaan"
-                            class="w-10 h-10 object-contain">
-                        <p class="h-fit px-3">PT. Kimia Farma</p>
-                    </div>
-                    <div class="font-bold text-lg mt-2">
-                        <p>Frontend Web Developer</p>
-                    </div>
-                    <p class="text-sm text-gray-600">Jakarta</p>
-                </div>
-            </button>
-
-            <button onclick="window.location='{{ route('mahasiswa.detail-perusahaan') }}'"
-                class="text-black text-base font-medium no-underline w-full text-left">
-                <div class="card-body bg-white hover:bg-gray-100 transition-colors rounded-md">
-                    <div class="flex align-middle items-center">
-                        <img src="{{ asset('logo-perusahaan.png') }}" alt="Logo Perusahaan"
-                            class="w-10 h-10 object-contain">
-                        <p class="h-fit px-3">PT. Kimia Farma</p>
-                    </div>
-                    <div class="font-bold text-lg mt-2">
-                        <p>Frontend Web Developer</p>
-                    </div>
-                    <p class="text-sm text-gray-600">Jakarta</p>
-                </div>
-            </button>
-
-            <button onclick="window.location='{{ route('mahasiswa.detail-perusahaan') }}'"
-                class="text-black text-base font-medium no-underline w-full text-left">
-                <div class="card-body bg-white hover:bg-gray-100 transition-colors rounded-md">
-                    <div class="flex align-middle items-center">
-                        <img src="{{ asset('logo-perusahaan.png') }}" alt="Logo Perusahaan"
-                            class="w-10 h-10 object-contain">
-                        <p class="h-fit px-3">PT. Kimia Farma</p>
-                    </div>
-                    <div class="font-bold text-lg mt-2">
-                        <p>Frontend Web Developer</p>
-                    </div>
-                    <p class="text-sm text-gray-600">Jakarta</p>
-                </div>
-            </button>
-
-            <button onclick="window.location='{{ route('mahasiswa.detail-perusahaan') }}'"
-                class="text-black text-base font-medium no-underline w-full text-left">
-                <div class="card-body bg-white hover:bg-gray-100 transition-colors rounded-md">
-                    <div class="flex align-middle items-center">
-                        <img src="{{ asset('logo-perusahaan.png') }}" alt="Logo Perusahaan"
-                            class="w-10 h-10 object-contain">
-                        <p class="h-fit px-3">PT. Kimia Farma</p>
-                    </div>
-                    <div class="font-bold text-lg mt-2">
-                        <p>Frontend Web Developer</p>
-                    </div>
-                    <p class="text-sm text-gray-600">Jakarta</p>
-                </div>
-            </button>
-
-            <button onclick="window.location='{{ route('mahasiswa.detail-perusahaan') }}'"
-                class="text-black text-base font-medium no-underline w-full text-left">
-                <div class="card-body bg-white hover:bg-gray-100 transition-colors rounded-md">
-                    <div class="flex align-middle items-center">
-                        <img src="{{ asset('logo-perusahaan.png') }}" alt="Logo Perusahaan"
-                            class="w-10 h-10 object-contain">
-                        <p class="h-fit px-3">PT. Kimia Farma</p>
-                    </div>
-                    <div class="font-bold text-lg mt-2">
-                        <p>Frontend Web Developer</p>
-                    </div>
-                    <p class="text-sm text-gray-600">Jakarta</p>
-                </div>
-            </button>
-
-            <button onclick="window.location='{{ route('mahasiswa.detail-perusahaan') }}'"
-                class="text-black text-base font-medium no-underline w-full text-left">
-                <div class="card-body bg-white hover:bg-gray-100 transition-colors rounded-md">
-                    <div class="flex align-middle items-center">
-                        <img src="{{ asset('logo-perusahaan.png') }}" alt="Logo Perusahaan"
-                            class="w-10 h-10 object-contain">
-                        <p class="h-fit px-3">PT. Kimia Farma</p>
-                    </div>
-                    <div class="font-bold text-lg mt-2">
-                        <p>Frontend Web Developer</p>
-                    </div>
-                    <p class="text-sm text-gray-600">Jakarta</p>
-                </div>
-            </button>
-
-            <button onclick="window.location='{{ route('mahasiswa.detail-perusahaan') }}'"
-                class="text-black text-base font-medium no-underline w-full text-left">
-                <div class="card-body bg-white hover:bg-gray-100 transition-colors rounded-md">
-                    <div class="flex align-middle items-center">
-                        <img src="{{ asset('logo-perusahaan.png') }}" alt="Logo Perusahaan"
-                            class="w-10 h-10 object-contain">
-                        <p class="h-fit px-3">PT. Kimia Farma</p>
-                    </div>
-                    <div class="font-bold text-lg mt-2">
-                        <p>Frontend Web Developer</p>
-                    </div>
-                    <p class="text-sm text-gray-600">Jakarta</p>
-                </div>
-            </button>
+            </div>
+            @endfor
         </div>
     </div>
 </div>
