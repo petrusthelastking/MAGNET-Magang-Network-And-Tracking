cb1bc4a49c773aa520f5f7d3939b75b40f0e56a2
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2025-06-04 19:30:03 +0700

    refactor: remove table `kriteria_reputasi_perusahaan`

--- database/factories/PreferensiMahasiswaFactory.php
+++ database/factories/PreferensiMahasiswaFactory.php
@@ -7,7 +7,6 @@ use App\Models\KriteriaJenisMagang;
 use App\Models\KriteriaLokasiMagang;
 use App\Models\KriteriaOpenRemote;
 use App\Models\KriteriaPekerjaan;
-use App\Models\KriteriaReputasiPerusahaan;
 use App\Models\Mahasiswa;
 use App\Models\PreferensiMahasiswa;
 use Illuminate\Database\Eloquent\Factories\Factory;
@@ -31,7 +30,6 @@ class PreferensiMahasiswaFactory extends Factory
             'kriteria_pekerjaan_id' => KriteriaPekerjaan::factory(),
             'kriteria_bidang_industri_id' => KriteriaBidangIndustri::factory(),
             'kriteria_lokasi_magang_id' => KriteriaLokasiMagang::factory(),
-            'kriteria_reputasi_perusahaan_id' => KriteriaReputasiPerusahaan::factory(),
             'kriteria_jenis_magang_id' => KriteriaJenisMagang::factory(),
             'kriteria_open_remote_id' => KriteriaOpenRemote::factory(),
         ];
