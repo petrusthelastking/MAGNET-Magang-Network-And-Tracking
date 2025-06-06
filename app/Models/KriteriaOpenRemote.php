1843aaa79c9250c373e45d723515733be3b12cbf
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2025-06-06 23:08:21 +0700

    feat: add new column `mahasiswa_id` for all related `kriteria_*` tables

--- app/Models/KriteriaOpenRemote.php
+++ app/Models/KriteriaOpenRemote.php
@@ -2,17 +2,19 @@
 
 namespace App\Models;
 
+use App\Traits\BaseKriteriaModel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class KriteriaOpenRemote extends Model
 {
-    use HasFactory;
+    use HasFactory, BaseKriteriaModel;
 
     protected $table = 'kriteria_open_remote';
 
     protected $fillable = [
         'open_remote',
+        'mahasiswa_id',
         'rank',
         'bobot'
     ];
