3357feab0eef45817027092ac8e08b264eda96c3
saalsabl <aisyahnzkk@gmail.com>
2025-06-09 11:43:39 +0700

    Template Surat Izin Magang

---
+++ resources/views/templates/pdf/surat-izin-magang.blade.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Surat Izin Magang</title>
+    <style>
+        body {
+            font-family: DejaVu Sans, sans-serif;
+            line-height: 1.6;
+            font-size: 14px;
+            color: #333;
+            margin: 40px;
+        }
+
+        h1,
+        h2,
+        h3 { 
+            margin-bottom: 5px;
+        }
+
+        .section {
+            margin-top: 25px;
+        }
+
+        .header {
+            border-bottom: 2px solid #000;
+            padding-bottom: 10px;
+            margin-bottom: 20px;
+        }
+
+        .contact-info p {
+            margin: 2px 0;
+        }
+
+        .job,
+        .edu {
+            margin-bottom: 15px;
+        }
+
+        .job h4,
+        .edu h4 {
+            margin: 0;
+        }
+
+        .job p,
+        .edu p {
+            margin: 2px 0;
+        }
+    </style>
+</head>
+
+<body>
+
+    <div class="header">
+        <h2>KEMENTRIAN PENDIDIKAN DAN KEBUDAYAAN</h2>
+        <h1>POLITEKNIK NEGERI MALANG</h1>
+        <p>Jl. Soekarno Hatta No.9, Jatimulyo, Kec. Lowokwaru, Kota Malang, Jawa Timur 65141</p>
+        <p>Telp (0341) 404424 - 404425 Fax (0341) 404420</p>
+        <p>http://www.polinema.ac.id</p>
+    </div>
+
+    <div class="content">
+        <p>Nomor&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: 123/PL-POLINEMA/VI/2025</p>
+        <p>Hal&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: Permohonan Izin Magang</p>
+        <p>Lampiran&nbsp;: -</p>
+        <br>
+
+        <p>Kepada Yth.</p>
+        <p>HRD Manager PT. Bisa</p>
+        <p>di Tempat</p>
+        <br>
+
+        <p>Dengan hormat,</p>
+        <p>Bersama surat ini, kami mengajukan permohonan izin magang bagi mahasiswa kami dari Politeknik Negeri Malang untuk melaksanakan kegiatan magang di perusahaan yang Bapak/Ibu pimpin. Adapun data mahasiswa tersebut adalah sebagai berikut:</p>
+
+        <ul>
+            <p>Nama&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: [Nama Mahasiswa]</p>
+            <p>NIM&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: [Nomor Induk Mahasiswa]</p>
+            <p>Program Studi&nbsp;: [Nama Program Studi]</p>
+            <p>Jurusan&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: [Nama Jurusan]</p>
+            <p>Semester&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: [Semester]</p>
+            <p>Waktu Magang&nbsp;&nbsp;: [Tanggal Mulai] s.d. [Tanggal Selesai]</p>
+        </ul>
+
+         <p>Kegiatan magang ini merupakan bagian dari kurikulum pembelajaran yang bertujuan untuk menambah wawasan dan pengalaman mahasiswa di dunia industri.</p>
+
+        <p>Demikian surat ini kami sampaikan. Atas perhatian dan kerja sama Bapak/Ibu, kami ucapkan terima kasih.</p>
+    </div>
+
+    <div class="signature">
+        <p>Malang, [Tanggal Surat]</p>
+        <p>Hormat kami,</p>
+        <p>Koordinator Magang</p>
+        <br><br><br>
+        <p><strong>[Nama Dosen Pembimbing]</strong></p>
+        <p>NIP. [Nomor Induk Pegawai]</p>
+    </div>
+</body>
+
+</html>
