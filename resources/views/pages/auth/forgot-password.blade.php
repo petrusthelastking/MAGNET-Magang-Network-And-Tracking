6defe43309c0450f6b03b80aad77017019ed1afe
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2025-06-11 19:29:48 +0700

    remove component `auth-session-status`

--- resources/views/pages/auth/forgot-password.blade.php
+++ resources/views/pages/auth/forgot-password.blade.php
@@ -25,9 +25,6 @@ new #[Layout('components.layouts.guest.with-navbar')] class extends Component {
 <div class="flex flex-col gap-6">
     <x-auth-header :title="__('Forgot password')" :description="__('Enter your email to receive a password reset link')" />
 
-    <!-- Session Status -->
-    <x-auth-session-status class="text-center" :status="session('status')" />
-
     <form wire:submit="sendPasswordResetLink" class="flex flex-col gap-6">
         <!-- Email Address -->
         <flux:input
