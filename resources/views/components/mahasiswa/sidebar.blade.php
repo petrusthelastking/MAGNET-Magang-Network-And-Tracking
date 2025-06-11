ea83dc11ba0296787c56bd8abef7baf8f119dc9c
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2025-06-11 10:08:25 +0700

    feat: add new attribute `wire:navigate` for smooth navigation

--- resources/views/components/mahasiswa/sidebar.blade.php
+++ resources/views/components/mahasiswa/sidebar.blade.php
@@ -3,43 +3,66 @@
 
     <flux:sidebar.toggle class="lg:hidden" icon="x-mark" />
 
-    <a href="{{ route('guest.landing-page') }}">
+    <a href="{{ route('guest.landing-page') }}" wire:navigate>
         <h1 class="text-3xl font-black ps-2 mx-4 text-center text-magnet-sky-teal">MAGNET</h1>
     </a>
 
     <flux:navlist variant="outline">
-        <flux:navlist.item icon="home" href="{{ route('dashboard') }}" :current="request()->is('dashboard')">
+        <flux:navlist.item
+            icon="home"
+            href="{{ route('dashboard') }}"
+            :current="request()->is('dashboard')"
+            wire:navigate>
             Dashboard
         </flux:navlist.item>
 
-        <flux:navlist.item icon="flask-conical" href="{{ route('mahasiswa.pengajuan-magang') }}"
-            :current="request()->is('pengajuan-magang')">
+        <flux:navlist.item
+            icon="flask-conical"
+            href="{{ route('mahasiswa.pengajuan-magang') }}"
+            :current="request()->is('pengajuan-magang')"
+            wire:navigate>
             Pengajuan Magang
         </flux:navlist.item>
 
-        <flux:navlist.item icon="message-square-more" href="{{ route('mahasiswa.konsul-dospem') }}"
-            :current="request()->is('konsul-dospem')" class="whitespace-normal break-words">
+        <flux:navlist.item
+            icon="message-square-more"
+            href="{{ route('mahasiswa.konsul-dospem') }}"
+            :current="request()->is('konsul-dospem')"
+            class="whitespace-normal break-words"
+            wire:navigate>
             Konsultasi Dosen <br>
             Pembimbing
         </flux:navlist.item>
 
-        <flux:navlist.item icon="battery-medium" href="{{ route('mahasiswa.pembaruan-status') }}"
-            :current="request()->is('pembaruan-status')">
+        <flux:navlist.item
+            icon="battery-medium"
+            href="{{ route('mahasiswa.pembaruan-status') }}"
+            :current="request()->is('pembaruan-status')"
+            wire:navigate>
             Pembaruan Status
         </flux:navlist.item>
 
-        <flux:navlist.item icon="file-chart-column-increasing" href="{{ route('mahasiswa.log-mahasiswa') }}"
-            :current="request()->is('log-mahasiswa')">
+        <flux:navlist.item
+            icon="file-chart-column-increasing"
+            href="{{ route('mahasiswa.log-mahasiswa') }}"
+            :current="request()->is('log-mahasiswa')"
+            wire:navigate>
             Log Magang
         </flux:navlist.item>
 
-        <flux:navlist.item icon="message-circle" href="{{ route('mahasiswa.saran-dari-dosen') }}"
-            :current="request()->is('saran-dari-dosen')">
+        <flux:navlist.item
+            icon="message-circle"
+            href="{{ route('mahasiswa.saran-dari-dosen') }}"
+            :current="request()->is('saran-dari-dosen')"
+            wire:navigate>
             Saran dari<br>
             Dosen
         </flux:navlist.item>
-        <flux:navlist.item icon="history" href="{{ route('mahasiswa.riwayat-rekomendasi') }}"
-            :current="request()->is('riwayat-rekomendasi')">
+        <flux:navlist.item
+            icon="history"
+            href="{{ route('mahasiswa.riwayat-rekomendasi') }}"
+            :current="request()->is('riwayat-rekomendasi')"
+            wire:navigate>
             Riwayat Rekomendasi <br>
             Magang
         </flux:navlist.item>
@@ -54,7 +77,7 @@
             <flux:navlist variant="outline" class="w-full">
                 <flux:button variant="ghost" class="flex my-2">
                     <flux:avatar src="https://unavatar.io/x/calebporzio" class="mx-auto" />
-                    <flux:navlist.item href="{{ route('profile') }}" class="text-black!">
+                    <flux:navlist.item href="{{ route('profile') }}" class="text-black!" wire:navigate>
                         <div class="text-base leading-6 font-normal">{{ auth('mahasiswa')->user()->nama }}</div>
                         <div class="text-xs leading-4 font-medium">{{ auth('mahasiswa')->user()->nim }}</div>
                     </flux:navlist.item>
