b826a6048887854fce1d8c0f160b483eb5ea941a
Dimas0824 <143877889+Dimas0824@users.noreply.github.com>
2025-06-19 20:49:14 +0700

    feat: Add 'Semua' option to various enums and update related views for consistency

--- database/seeders/PekerjaanSeeder.php
+++ database/seeders/PekerjaanSeeder.php
@@ -14,6 +14,7 @@ class PekerjaanSeeder extends Seeder
     public function run(): void
     {
         $jobs = [
+            'Semua',
             'Software Engineer',
             'Data Engineer',
             'Backend Developer',
